f253a30790999809cde839ef32b692bd6805b006
theo-be <belliere.theo@outlook.fr>
2021-04-30 13:23:18 +0200

    menu fonctionnel

--- sources/main.c
+++ sources/main.c
@@ -3,17 +3,25 @@
 #include <string.h>
 #include <math.h>
 #include <string.h>
-#include "cocktail.h"
-#include "cocktail.c"
-#include <windows.h>
+
+
+#include "menu.h"
 
 
 int main(int argc, char** argv) {
- boisson_struc* stock = remplirstock();
+    // boisson_struc* stock = remplirstock();
     //printf("%d\n",stock[0].quantite);
     //commande(stock,0,2,0);
 
-   //affichage();
-   affichage_type(122,2,stock);
+    // affichage();
+    // affichage_type(122,2,stock);
+
+    char quitter = 0;
+
+
+    do {
+        quitter = afficherMenu();
+    } while (quitter != 'q');
+
     return 0;
 }
